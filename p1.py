4737597919656e79805ef5f64b8cc3a024b7cf7f
zoro <zoro@gmail.com>
2023-08-16 10:21:26 +0530

    p1

---
+++ p1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.read_csv("C:/Users/SPTINT-09/Desktop/hyeok/Iris.csv")
+print(data)
+print(data.tail(10))
+print(data.head(5))
+print(data["PetalWidthCm"])
+print(data.describe())
+print(data.info())
+print(data.dtypes)
+print(data.count())
